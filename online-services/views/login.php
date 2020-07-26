66affe3dc53f05bb2b5daf8043bdf6f3ca9aa16d
rogeryow <rogerpantilyow@gmail.com>
2020-07-26 11:35:47 +0800

    first commit

---
+++ online-services/views/login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Online services</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/form.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+	<!-- nav -->
+	<nav class="main-nav">
+		<div class="main-nav-inner">
+			<!-- <img src="assets/img/logo/logo.png" alt="Online Services" class="logo"> -->
+			<label class="logo"><i class="fa fa-bicycle"></i> Services on the Go</label>
+
+			<ul class="main-menu">
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Contact us</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Support</a></li>
+			</ul>
+
+			<ul class="right-menu">
+				<li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+				<li><a href="#"><i class="fa fa-user-circle"></i></a></li>
+			</ul>
+
+			<div class="menu-btn">
+				<i class="fa fa-bars fa-2x"></i>
+			</div>
+		</div>
+	</nav>
+
+	<section class="login-section">
+		<div class="login-form">
+			<h2>Login</h2>
+			<label class="label">Username</label>
+			<input class="input" type="" name="" placeholder="Username or Password">
+			<label class="label">Password</label>
+			<input class="input" type="" name="" placeholder="Password">
+			<button class="btn-form btn-red">Login</button>
+			<hr class="divider">
+			<p class="form-text">Don't have an account? Sign up as</p>
+			<p class="group-link">
+				<a href="#">User</a>
+				or
+				<a href="#">Vendor</a>
+			</p>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	<footer class="footer">
+		<div class="footer-inner">
+			<div>
+				<label class="logo"><i class="fa fa-bicycle"></i> Services on the Go &copy; 2020 All rights reserved.</label>
+			</div>
+			<ul>
+				<li><a href="#">Sitemap</a></li>
+				<li><a href="#">Privacy & cookies</a></li>
+				<li><a href="#">Terms of use</a></li>
+				<li><a href="#">Trademarks</a></li>
+				<li><a href="#">Safety & eco</a></li>
+				<li><a href="#">About our ads</a></li>
+				<li><a href="#">&copy; Services on the Go 2020</a></li>
+			</ul>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ online-services/views/register.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Online services</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/form.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+	<!-- nav -->
+	<nav class="main-nav">
+		<div class="main-nav-inner">
+			<!-- <img src="assets/img/logo/logo.png" alt="Online Services" class="logo"> -->
+			<label class="logo"><i class="fa fa-bicycle"></i> Services on the Go</label>
+
+			<ul class="main-menu">
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Contact us</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Support</a></li>
+			</ul>
+
+			<ul class="right-menu">
+				<li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+				<li><a href="#"><i class="fa fa-user-circle"></i></a></li>
+			</ul>
+
+			<div class="menu-btn">
+				<i class="fa fa-bars fa-2x"></i>
+			</div>
+		</div>
+	</nav>
+
+	<section class="login-section">
+		<div class="login-form">
+			<h2>Sign up</h2>
+			<label class="label">Name</label>
+			<input class="input" type="" name="" placeholder="Your name">
+			<label class="label">Address</label>
+			<input class="input" type="" name="" placeholder="Enter your address">
+			<label class="label">Phone number</label>
+			<input class="input" type="" name="" placeholder="Enter your phone number">
+			<label class="label">Password</label>
+			<input class="input" type="" name="" placeholder="Enter your password">
+			<label class="label">Confirm Password</label>
+			<input class="input" type="" name="" placeholder="Enter again to confirm">
+			<button class="btn-form btn-red">Login</button>
+			<hr class="divider">
+			<p class="form-text">
+			Already have account?</p>
+			<p class="group-link">
+				<a href="#">Login</a>
+			</p>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	<footer class="footer">
+		<div class="footer-inner">
+			<div>
+				<i class="fa fa-globe fa-2x"></i> English (United States)
+			</div>
+			<ul>
+				<li><a href="#">Sitemap</a></li>
+				<li><a href="#">Privacy & cookies</a></li>
+				<li><a href="#">Terms of use</a></li>
+				<li><a href="#">Trademarks</a></li>
+				<li><a href="#">Safety & eco</a></li>
+				<li><a href="#">About our ads</a></li>
+				<li><a href="#">&copy; Services on the Go 2020</a></li>
+			</ul>
+		</div>
+	</footer>
+</body>
+</html>
